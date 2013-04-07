8a26c6f7669d0291886eabdea16f64e57127c3d2
Samantha John <samantha.m.john@gmail.com>
2013-04-07 15:14:47 -0400

    Rearrange files

---
+++ GymSchedule/controllers/GSViewController.h
@@ -0,0 +1,13 @@
+//
+//  GSViewController.h
+//  GymSchedule
+//
+//  Created by Samantha John on 4/7/13.
+//  Copyright (c) 2013 Hopscotch Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSViewController : UIViewController
+
+@end
