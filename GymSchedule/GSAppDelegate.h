5757f0c09b1a7e97f656a2a2efca0d3b6922899f
Samantha John <samantha.m.john@gmail.com>
2013-04-07 15:03:31 -0400

    Initial Commit

---
+++ GymSchedule/GSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  GSAppDelegate.h
+//  GymSchedule
+//
+//  Created by Samantha John on 4/7/13.
+//  Copyright (c) 2013 Hopscotch Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
